359f778ddb536dca6a3e71784352821d24d3f848
Valentin Berthome <valentin.berthome@gmail.com>
2015-09-04 15:10:42 +0200

    Développement du fonctionnement des formulaires

---
+++ wp-content/themes/avignon/email/completed.php
@@ -0,0 +1,13 @@
+<h1>Congratulations!</h1>
+
+<p>We are thrilled to welcome you this Summer to the Institut d’Avignon!</p>
+
+<h2>Informations for admitted students</h2>
+
+<p>Here are some helpful tips on the best way to prepare for your trip to France: [url page student info]</p>
+
+<h2>Housing</h2>
+
+<p>Please complete the housing form ASAP. Some options have limited availability and students’ preferences are handled on a first-come, first-served basis: [url formulaire logement]</p>
+
+<p>Institut d’Avignon</p>
---
+++ wp-content/themes/avignon/email/refused.php
@@ -0,0 +1,9 @@
+<h1>We are sorry!</h1>
+
+<p>Your application has been rejected. Let us explain why:</p>
+
+<?php the_field( 'applicant_reason' ) ?>
+
+<p>Feel free to contact us for a more detailed account.</p>
+
+<p>Institut d’Avignon</p>
