b5455d194e079e2c78d3efe6718ffb068f3c2881
elisabeth <elisabeth@Chiot.local>
2015-09-04 15:58:42 +0200

    Conflit

--- wp-content/themes/avignon/email/refused.php
+++
@@ -1,9 +0,0 @@
-<h1>We are sorry!</h1>
-
-<p>Your application has been rejected. Let us explain why:</p>
-
-<?php echo $refused_reasons ?>
-
-<p>Feel free to contact us for a more detailed account.</p>
-
-<p>Institut d’Avignon</p>
