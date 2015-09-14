362af7a4ec74d1ce8fc33b4835c396982db1a7d6
Valentin Berthome <valentin.berthome@gmail.com>
2015-09-14 10:31:54 +0200

    Erreur d'envoi du jeton

--- wp-content/themes/avignon/email/reference.php
+++ wp-content/themes/avignon/email/reference.php
@@ -2,7 +2,7 @@
 
 <p>According to our records, you are willing to recommend <?php echo $data['first_name'] ?> <?php echo $data['last_name'] ?> for the Institut d’Avignon (Bryn Mawr Summer Institute in French Studies). Please complete this recommendation by March 1st:</p>
 
-<p><?php echo add_query_arg( array( 'token' => $data['token'], 'reference' => $reference ), get_permalink( AVIGNON_RECOMMENDATION_PAGE_ID ) ) ?></p>
+<p><?php echo add_query_arg( array( 'token' => $token, 'reference' => $reference ), get_permalink( AVIGNON_RECOMMENDATION_PAGE_ID ) ) ?></p>
 
 <p>Thank you for your consideration,</p>
 
