7787ed01f5ebfac7c33223fb149c15f68ba78b18
Valentin Berthome <valentin.berthome@gmail.com>
2015-09-18 09:46:02 +0200

    Espace vide et jeton dans le formulaire de santé

--- wp-content/themes/avignon/apply-success.php
+++ wp-content/themes/avignon/apply-success.php
@@ -41,12 +41,12 @@ get_header(); ?>
                         </div>
                     </aside><section>
                         <h2>Your application has been submitted. Please complete these steps in order to be officially enrolled:</h2>
-                        
+
                         <p>
-                            Your recommenders (French professor and dean, or graduate advisor) should have received a recommendation request by email. 
+                            Your recommenders (French professor and dean, or graduate advisor) should have received a recommendation request by email.
                             Please confirm that they have received a request from us. If not, they may access the information via this link:
                         </p>
-                        
+
                         <ul>
                             <li>
                                 <a href="<?php echo add_query_arg( array( 'token' => $_GET['token'], 'reference' => 1 ), get_permalink( AVIGNON_RECOMMENDATION_PAGE_ID ) ) ?>">
@@ -59,29 +59,29 @@ get_header(); ?>
                                 </a>
                             </li>
                         </ul>
-                        
+
                         <p>NB: please don’t forget to have your official transcripts mailed to us! (mailing address: see below)</p>
 
                         <p>
-                            Once you have provided all required materials, including your recommendations and transcripts, 
-                            your application will be considered for enrollment. 
+                            Once you have provided all required materials, including your recommendations and transcripts,
+                            your application will be considered for enrollment.
                             You will be notified by email whether your application has been accepted or rejected.
                         </p>
 
                         <p>
-                            If your application has been accepted, you will still need to provide us with a signed health evaluation 
-                            from your primary care doctor in order for you to be officially enrolled in the program. 
-                            Here is the form to be completed by you and your doctor: 
+                            If your application has been accepted, you will still need to provide us with a signed health evaluation
+                            from your primary care doctor in order for you to be officially enrolled in the program.
+                            Here is the form to be completed by you and your doctor:
                         </p>
 
                         <ul>
                             <li>
-                                <a href="<?php echo add_query_arg( 'token', $token, get_permalink( AVIGNON_HEALTH_EVALUATION_PAGE_ID ) ) ?>">
+                                <a href="<?php echo add_query_arg( 'token', $_GET['token'], get_permalink( AVIGNON_HEALTH_EVALUATION_PAGE_ID ) ) ?>">
                                     Health evaluation form
                                 </a>
                             </li>
                         </ul>
-                         
+
                         <p>
                             Once this form is submitted, you will receive a link to upload this document.
                         </p>
